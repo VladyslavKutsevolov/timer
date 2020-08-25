89484dad5d7c5b3f99cbba8af22c799b3eab3821
Vladyslav <kytsevolov@gmail.com>
2020-08-25 14:42:16 -0600

    simple alarm

--- timer.js
+++ timer.js
@@ -3,9 +3,12 @@ const argv = process.argv.slice(2);
 const alarm = (arrayOfseconds) => {
   arrayOfseconds.sort();
   for (let delay of arrayOfseconds) {
+    if (!delay || !Number(delay)) {
+      console.log('Input should be only numbers');
+      return;
+    }
     delay = delay * 1000;
     setTimeout(() => process.stdout.write('\x07'), delay);
   }
 };
-
 alarm(argv);
